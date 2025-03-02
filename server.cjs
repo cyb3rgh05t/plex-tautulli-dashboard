08494c2a28c68dfaad6c4c031a6b288d47883615
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2025-03-02 19:10:12 +0100

    server fix

--- server.cjs
+++ server.cjs
@@ -8,7 +8,6 @@ const { getFormats, saveFormats } = require("./src/utils/formatStore.cjs");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
-import { appVersion } from "../../../version";
 
 // Constants and Configuration
 const SAVED_SECTIONS_PATH = path.join(
@@ -2027,7 +2026,6 @@ const formatConfig = (config) => {
         : value || "Not set";
     output += `\t├── ${key}: ${displayValue},\n`;
   });
-  output += "├──";
   return output;
 };
 
@@ -2035,7 +2033,7 @@ const serverBanner = `
 ╔════════════════════════════════════════════════════╗
 ║                                                    ║
 ║            Plex & Tautulli Dashboard               ║
-║                 Version ${appVersion}              ║
+║                                                    ║
 ║                                                    ║
 ║                 by cyb3rgh05t                      ║
 ╚════════════════════════════════════════════════════╝`;
@@ -2072,7 +2070,7 @@ app.listen(PORT, "0.0.0.0", () => {
   // Changed from localhost to 0.0.0.0
   console.clear();
   console.log(serverBanner);
-  console.log(`Version: ${appVersion}`);
+  //console.log(`Version: ${appVersion}`);
   console.log("\n🚀 Server Information:");
   console.log("├── Status: Running");
   console.log(`├── Listening on: ${process.env.VITE_API_BASE_URL}`);
